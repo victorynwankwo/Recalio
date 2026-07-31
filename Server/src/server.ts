17cbe089ab5c74d21bb22dbc2bea4e89aaba1ae7
Victory Nwankwo <nwankwovic211@gmail.com>
2026-07-31 10:44:45 -0400

    Trying to setup my frontend

---
+++ Server/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+
+
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Landinng from "./Pages/Landing"
+
+const App = () => {
+  return (
+    <div>
+      <Landing />
+    </div>
+  )
+}
+
+export default App
